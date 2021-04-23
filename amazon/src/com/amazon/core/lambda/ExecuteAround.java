674933aedca2532c4363e99d6d248f61cc182bdb
Alokit Jindal <iamalokit@gmail.com>
2021-04-23 13:47:50 +0530

    Lambdas

---
+++ amazon/src/com/amazon/core/lambda/ExecuteAround.java
@@ -0,0 +1,38 @@
+package com.amazon.core.lambda;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExecuteAround {
+	public static void main(String[] args) throws IOException {
+		String result = processFileLimited();
+		System.out.println(result);
+
+		System.out.println("---");
+
+		String oneLine = processFile((BufferedReader b) -> b.readLine());
+		System.out.println(oneLine);
+
+		String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
+		System.out.println(twoLines);
+	}
+
+	private static String processFileLimited() throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+			return br.readLine();
+		}
+	}
+
+	public static String processFile(BufferedReaderProcessor p) throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+			return p.process(br);
+		}
+
+	}
+
+	public interface BufferedReaderProcessor {
+		public String process(BufferedReader b) throws IOException;
+
+	}
+}
