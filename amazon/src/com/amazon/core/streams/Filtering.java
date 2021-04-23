59b073cf3e1449ad6191b3d317b52f02b4ed4722
Alokit Jindal <iamalokit@gmail.com>
2021-04-23 15:24:30 +0530

    Streams and filter

---
+++ amazon/src/com/amazon/core/streams/Filtering.java
@@ -0,0 +1,23 @@
+package com.amazon.core.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amazon.core.lambda.Dish;
+
+public class Filtering {
+	public static void main(String[] args) {
+		List<Dish> vegetarianMenu = Dish.menu.stream().filter(Dish::isVegetarian).collect(Collectors.toList());
+		vegetarianMenu.forEach(System.out::println);
+		
+		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+		numbers.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
+		
+		List<Dish> dishesLimit3 = Dish.menu.stream().filter(d -> d.getCalories() > 300).limit(3).collect(Collectors.toList());
+		dishesLimit3.forEach(System.out::println);
+		
+		List<Dish> dishesSkip2 = Dish.menu.stream().filter(d -> d.getCalories() > 300).skip(2).collect(Collectors.toList());
+		dishesSkip2.forEach(System.out::println);
+	}
+}
