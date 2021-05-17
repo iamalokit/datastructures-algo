5b032140e3eee7932731eeab3aea65d7a532a075
Alokit Jindal <iamalokit@gmail.com>
2021-05-17 12:23:26 +0530

    Online banking sample designs using lambda and abstraction

---
+++ amazon/src/com/amazon/core/lambda/design/OnlineBankingLambda.java
@@ -0,0 +1,26 @@
+package com.amazon.core.lambda.design;
+
+import java.util.function.Consumer;
+
+public class OnlineBankingLambda {
+	
+	public static void main(String[] args) {
+        new OnlineBankingLambda().processCustomer(1337, (Customer c) -> System.out.println("Hello!"));
+    }
+	
+	public void processCustomer(int id, Consumer<Customer> makeCustomerHappy) {
+		Customer c = Database.getCustomerWithId(id);
+		makeCustomerHappy.accept(c);
+	}
+
+	// dummy Customer class
+	static private class Customer {
+	}
+
+	// dummy Datbase class
+	static private class Database {
+		static Customer getCustomerWithId(int id) {
+			return new Customer();
+		}
+	}
+}
