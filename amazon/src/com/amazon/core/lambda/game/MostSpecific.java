7de340b177e280c5c6373cee64b8974fe2f520ee
Alokit Jindal <iamalokit@gmail.com>
2021-05-20 21:48:45 +0530

    most specific interface method

---
+++ amazon/src/com/amazon/core/lambda/game/MostSpecific.java
@@ -0,0 +1,43 @@
+package com.amazon.core.lambda.game;
+
+public class MostSpecific {
+	public static void main(String[] args) {
+		new C().hello();
+		new E().hello();
+		new D().hello();
+		new F().hello();
+		new G().hello();
+	}
+
+	static interface A {
+		public default void hello() {
+			System.out.println("Hello from A");
+		}
+	}
+
+	static interface B extends A {
+		public default void hello() {
+			System.out.println("Hello from B");
+		}
+	}
+
+	static class C implements B, A {
+
+	}
+
+	static class D implements A {
+	}
+
+	static class E extends D implements B, A {
+	}
+
+	static class F implements B, A {
+		public void hello() {
+			System.out.println("Hello from F");
+		}
+	}
+	
+    static class G extends F implements B, A{}
+
+
+}
